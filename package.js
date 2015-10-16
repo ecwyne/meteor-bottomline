a88774817888a587ce47e5243849a9b22c7292c4
Eric Wyne <ecwyne@gmail.com>
2015-10-16 12:16:49 -0700

    bump version

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
 	summary: "Bottomline api packaged for Meteor",
-	version: "0.0.1",
+	version: "0.0.2",
 	git: "http://github.com/ecwyne/meteor-bottomline",
 	name: 'ecwyne:bottomline'
 });
 
 Npm.depends({
-	bottomline: '0.0.1'
+	bottomline: '0.0.2'
 })
 
 Package.onUse(function(api) {
